94714b29b09158603eea50db9589348d74f462df
siddharthrai64 <siddharthrai64@gmail.com>
2023-11-28 23:56:13 +0530

    Api-key-remove

--- youtube-clone/src/utils/constant.js
+++ youtube-clone/src/utils/constant.js
@@ -1,3 +1,3 @@
-const GOOGLE_API_KEY = "AIzaSyAkA3qcFDRe4ez7rgSyhTDQjqpL6rXRjao";
+const GOOGLE_API_KEY = "";
 export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" + GOOGLE_API_KEY;
 export const YOUTUBE_SEARCH_API = "https://cors-anywhere.herokuapp.com/https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
